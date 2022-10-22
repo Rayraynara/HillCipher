3a7bc6cd41b715c614e19ba234257cf99522dd8a
Ray_dmo <raynaramartins.1999@gmail.com>
2022-10-22 19:35:04 -0300

    Criação da estrutura inicial

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// C# code to implement Hill Cipher
+using System;
+
+class HC
+{
+
+    
+    static void getKeyMatrix(String key,  int[,] keyMatrix)
+    {
+        
+    }
+
+    static void encrypt()
+    {
+        
+    }
+
+    static void HillCipher(String message, String key)
+    {
+
+        int[,] keyMatrix = new int[3, 3];
+        getKeyMatrix(key, keyMatrix);
+
+        Console.Write("Ciphertext: " + CipherText + "\n");
+    }
+
+    public static void Main(String[] args)
+    {
+        String message = "RAY";
+        String key = "HILLMAGIC";
+
+        HillCipher(message, key);
+    }
+}
+
