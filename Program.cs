91497a91de14bb92e91e96aef304cf60ca8212f8
Ray_dmo <raynaramartins.1999@gmail.com>
2022-11-05 12:37:48 -0300

    Conclusão do método de encrypt

--- Program.cs
+++ Program.cs
@@ -19,7 +19,22 @@ class HC
 
     static void encrypt(int[,] cipherMatrix, int[,] keyMatrix, int[,] messageVector)
     {
+        int x, i, j;
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 1; j++)
+            {
+                cipherMatrix[i, j] = 0;
 
+                for (x = 0; x < 3; x++)
+                {
+                    cipherMatrix[i, j] += keyMatrix[i, x] *
+                                        messageVector[x, j];
+                }
+
+                cipherMatrix[i, j] = cipherMatrix[i, j] % 26;
+            }
+        }
     }
 
     static void HillCipher(String message, String key)
@@ -27,6 +42,15 @@ class HC
 
         int[,] keyMatrix = new int[3, 3];
         getKeyMatrix(key, keyMatrix);
+
+        int[,] messageVector = new int[3, 1];
+
+        for (int i = 0; i < 3; i++)
+            messageVector[i, 0] = (message[i]) % 65;
+
+        int[,] cipherMatrix = new int[3, 1];
+
+        encrypt(cipherMatrix, keyMatrix, messageVector);
 
         String CipherText = "";
 
